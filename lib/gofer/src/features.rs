496d0c31cec4ea8707194cd6cf41e6e49447a6bf
Arto Bendiken <arto@bendiken.net>
2025-04-24 11:30:20 +0200

    Define feature flags and dependencies.

--- lib/gofer/src/features.rs
+++ lib/gofer/src/features.rs
@@ -2,6 +2,14 @@
 
 /// The set of features that are enabled in this build of the crate.
 pub static FEATURES: &[&str] = &[
+    #[cfg(feature = "data")]
+    "data",
+    #[cfg(feature = "file")]
+    "file",
+    #[cfg(feature = "http")]
+    "http",
+    #[cfg(feature = "https")]
+    "https",
     #[cfg(feature = "unstable")]
-    unstable,
+    "unstable",
 ];
